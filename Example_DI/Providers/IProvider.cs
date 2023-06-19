4b95fe8a7c25e6474cf35d1ef5df7103fd56934e
patrickfonseca208 <patrickfonseca208@gmail.com>
2023-06-18 23:41:29 -0300

    Update repository

--- Example_DI/Providers/IProvider.cs
+++ Example_DI/Providers/IProvider.cs
@@ -2,7 +2,7 @@
 
 namespace Example_DI.Providers
 {
-    // An example of a service that invokes "in a application of real world", a real client or a fake client - defined in appsettings
+    // An example of a service that invokes "in an application of real world", a real client or a fake client - defined in appsettings
     public interface IProvider
     {
         IEnumerable<User> GetUsers();
